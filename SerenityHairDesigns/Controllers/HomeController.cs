af76bd3f6b5756400562bc173d9371e95b97bdfa
Brooklyn Bressler <brooklynbressler@gmail.com>
2023-03-03 03:12:35 -0500

    Career Page Creation & Button Functionality

--- SerenityHairDesigns/Controllers/HomeController.cs
+++ SerenityHairDesigns/Controllers/HomeController.cs
@@ -13,5 +13,28 @@ namespace SerenityHairDesigns.Controllers
         {
             return View();
         }
+
+        // GET: Careers
+        public ActionResult Careers() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection col) {
+
+            if (col["btnCareers"].ToString() == "submit") return RedirectToAction("Careers");
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Careers(FormCollection col) {
+
+            if (col["btnHome"].ToString() == "submit") return RedirectToAction("Index");
+
+
+            return View();
+        }
     }
 }

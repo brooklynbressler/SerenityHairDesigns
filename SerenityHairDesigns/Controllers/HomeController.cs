294978dffe2f33c4d24b2a39651f58fb7b7a8cd4
RichardMiller <44434231+BMiller2k2@users.noreply.github.com>
2023-03-06 17:21:39 -0500

    changed layout and all pages

--- SerenityHairDesigns/Controllers/HomeController.cs
+++ SerenityHairDesigns/Controllers/HomeController.cs
@@ -25,32 +25,6 @@ namespace SerenityHairDesigns.Controllers
             return View();
         }
 
-        [HttpPost]
-        public ActionResult Index(FormCollection col) {
 
-            if (col["btnCareers"].ToString() == "submit") return RedirectToAction("Careers");
-
-            if (col["btnServices"].ToString() == "submit") return RedirectToAction("Services");
-
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Careers(FormCollection col) {
-
-            if (col["btnHome"].ToString() == "submit") return RedirectToAction("Index");
-
-
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Services(FormCollection col)
-        {
-
-            if (col["btnHome"].ToString() == "submit") return RedirectToAction("Index");
-
-            return View();
-        }
     }
 }

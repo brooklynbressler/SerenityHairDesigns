9f234023aeaeae83201f6b0b740488c22e3c93aa
RichardMiller <44434231+BMiller2k2@users.noreply.github.com>
2023-03-13 19:45:44 -0400

    bookNow page has full design just needs to be proceduerised

---
+++ SerenityHairDesigns/Controllers/BookNowController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SerenityHairDesigns.Controllers
+{
+    public class BookNowController : Controller
+    {
+        // GET: Home
+        public ActionResult Stylist1()
+        {
+            return View();
+        }
+
+        public ActionResult Stylist2()
+        {
+            return View();
+        }
+
+        public ActionResult Stylist3()
+        {
+            return View();
+        }
+
+        public ActionResult Stylist4()
+        {
+            return View();
+        }
+
+    }
+}

2b562ad885b8e759be16255f5815d065ae4c236c
Brooklyn Bressler <brooklynbressler@gmail.com>
2023-03-17 00:29:29 -0400

    More Schedule Now Changes

--- SerenityHairDesigns/Controllers/profileController.cs
+++ SerenityHairDesigns/Controllers/profileController.cs
@@ -14,15 +14,17 @@ namespace SerenityHairDesigns.Controllers
             return View();
         }
 
-		public ActionResult SignIn() {
-			web2.Models.User u = new web2.Models.User();
-			return View(u);
-		}
-
 		[HttpPost]
-		public ActionResult SignIn(FormCollection col) {
+		public ActionResult ScheduleLogin(FormCollection col) {
 			try {
 				web2.Models.User u = new web2.Models.User();
+
+				u.FirstName = col["FirstName"];
+				u.LastName = col["LastName"];
+				u.Email = col["Email"];
+				u.UserID = col["UserID"];
+				u.Password = col["Password"];
+				u.Gender = col["Genders"];
 
 				if (col["btnSubmit"] == "signin") {
 					u.UserID = col["UserID"];
@@ -39,37 +41,7 @@ namespace SerenityHairDesigns.Controllers
 						u.ActionType = web2.Models.User.ActionTypes.LoginFailed;
 					}
 				}
-				return View(u);
-			}
-			catch (Exception) {
-				web2.Models.User u = new web2.Models.User();
-				return View(u);
-			}
-		}
-
-		public ActionResult SignUp() {
-			web2.Models.User u = new web2.Models.User();
-			return View(u);
-		}
-
-		[HttpPost]
-		public ActionResult SignUp(FormCollection col) {
-			try {
-				web2.Models.User u = new web2.Models.User();
-
-				u.FirstName = col["FirstName"];
-				u.LastName = col["LastName"];
-				u.Email = col["Email"];
-				u.UserID = col["UserID"];
-				u.Password = col["Password"];
-				u.Gender = col["Genders"];
-
-				if (u.FirstName.Length == 0 || u.LastName.Length == 0 || u.Email.Length == 0 || u.UserID.Length == 0 || u.Password.Length == 0) {
-					u.ActionType = web2.Models.User.ActionTypes.RequiredFieldsMissing;
-					return View(u);
-				}
-				else {
-					if (col["btnSubmit"] == "signup") { //sign up button pressed
+				else if (col["btnSubmit"] == "signup") { //sign up button pressed
 						web2.Models.User.ActionTypes at = web2.Models.User.ActionTypes.NoType;
 						at = u.Save();
 						switch (at) {
@@ -81,17 +53,55 @@ namespace SerenityHairDesigns.Controllers
 								return View(u);
 								//break;
 						}
-					}
-					else {
-						return View(u);
-					}
 				}
+				return View(u);
 			}
 			catch (Exception) {
 				web2.Models.User u = new web2.Models.User();
 				return View(u);
 			}
 		}
+
+		//[HttpPost]
+		//public ActionResult ScheduleLogin(FormCollection col) {
+		//	try {
+		//		web2.Models.User u = new web2.Models.User();
+
+		//		u.FirstName = col["FirstName"];
+		//		u.LastName = col["LastName"];
+		//		u.Email = col["Email"];
+		//		u.UserID = col["UserID"];
+		//		u.Password = col["Password"];
+		//		u.Gender = col["Genders"];
+
+		//		if (u.FirstName.Length == 0 || u.LastName.Length == 0 || u.Email.Length == 0 || u.UserID.Length == 0 || u.Password.Length == 0) {
+		//			u.ActionType = web2.Models.User.ActionTypes.RequiredFieldsMissing;
+		//			return View(u);
+		//		}
+		//		else {
+		//			if (col["btnSubmit"] == "signup") { //sign up button pressed
+		//				web2.Models.User.ActionTypes at = web2.Models.User.ActionTypes.NoType;
+		//				at = u.Save();
+		//				switch (at) {
+		//					case web2.Models.User.ActionTypes.InsertSuccessful:
+		//						u.SaveUserSession();
+		//						return RedirectToAction("Index");
+		//					//break;
+		//					default:
+		//						return View(u);
+		//						//break;
+		//				}
+		//			}
+		//			else {
+		//				return View(u);
+		//			}
+		//		}
+		//	}
+		//	catch (Exception) {
+		//		web2.Models.User u = new web2.Models.User();
+		//		return View(u);
+		//	}
+		//}
 
 		public ActionResult SignOut() {
 			web2.Models.User u = new web2.Models.User();

e0b1b669fe751bbf61889e3e75fa789add3cede8
Brooklyn Bressler <brooklynbressler@gmail.com>
2023-03-22 20:56:19 -0400

    Book Now Changes, Added Employee.cs, Changed User.cs to Customer.cs

--- SerenityHairDesigns/Controllers/HomeController.cs
+++ SerenityHairDesigns/Controllers/HomeController.cs
@@ -1,12 +1,10 @@
-﻿using SerenityHairDesigns.Models;
+﻿using Microsoft.AspNetCore.Http;
+using SerenityHairDesigns.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Microsoft.AspNetCore.Http;
-
-
 
 namespace SerenityHairDesigns.Controllers
 {
@@ -15,8 +13,41 @@ namespace SerenityHairDesigns.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            Models.Customer u = new Models.Customer();
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(System.Web.Mvc.FormCollection col) {
+            try {
+                Models.Employee e = new Models.Employee();
+
+                if (col["btnSubmit"] == "signin") {
+                    e.strEmailAddress = col["strEmailAddress"];
+                    e.strPassword = col["strPassword"];
+
+                    e = e.LoginEmployee();
+                    if (e != null && e.intEmployeeID > 0) {
+                        e.SaveEmployeeSession();
+                        return RedirectToAction("ScheduleNowLoggedIn", "profile");
+                    }
+                    else {
+                        e = new Models.Employee();
+                        e.ActionType = Models.Employee.ActionTypes.LoginFailed;
+                    }
+                }
+                return View(e);
+            }
+            catch (Exception) {
+                Models.Customer u = new Models.Customer();
+                return View(u);
+            }
+        }
+
+        //public ActionResult EmployeeLoggedIn() {
+        //    Models.User u = new Models.User();
+        //    return View(u);
+        //}
 
         // GET: Careers
         public ActionResult Careers() { 
---
+++ SerenityHairDesigns/Models/Employee.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.IO;
+using System.ComponentModel.DataAnnotations;
+using SerenityHairDesigns.Models;
+
+namespace SerenityHairDesigns.Models {
+	public class Employee {
+
+        public long intEmployeeID = 0;
+        public string strFirstName = string.Empty;
+        public string strLastName = string.Empty;
+        public string strGender = string.Empty;
+        public string strPassword = string.Empty;
+
+        //[DataType(DataType.EmailAddress)]
+        public string strEmailAddress = string.Empty;
+
+        //[DataType(DataType.PhoneNumber)]
+        public string strPhoneNumber = string.Empty;
+
+        public ActionTypes ActionType = ActionTypes.NoType;
+        public Image UserImage;
+        public List<Image> Images;
+
+		public Employee LoginEmployee() {
+            try {
+                Database db = new Database();
+                return db.LoginEmployee(this);
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+        }
+
+        public bool IsEmployeeAuthenticated {
+            get {
+                if (intEmployeeID > 0) return true;
+                return false;
+            }
+        }
+
+        public Employee.ActionTypes SaveEmployee() {
+            try {
+                Database db = new Database();
+                if (intEmployeeID == 0) { //insert new user
+                    this.ActionType = db.InsertEmployee(this);
+                }
+                else {
+                    this.ActionType = db.UpdateEmployee(this);
+                }
+                return this.ActionType;
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+        }
+
+        public bool RemoveEmployeeSession() {
+            try {
+                HttpContext.Current.Session["CurrentUser"] = null;
+                return true;
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+        }
+
+        public Customer GetEmployeeSession() {
+            try {
+                Customer u = new Customer();
+                if (HttpContext.Current.Session["CurrentUser"] == null) {
+                    return u;
+                }
+                u = (Customer)HttpContext.Current.Session["CurrentUser"];
+                return u;
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+        }
+
+        public bool SaveEmployeeSession() {
+            try {
+                HttpContext.Current.Session["CurrentUser"] = this;
+                return true;
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+        }
+
+        public enum ActionTypes {
+            NoType = 0,
+            InsertSuccessful = 1,
+            UpdateSuccessful = 2,
+            DuplicateEmail = 3,
+            DuplicateUserID = 4,
+            Unknown = 5,
+            RequiredFieldsMissing = 6,
+            LoginFailed = 7
+        }
+    }
+}
